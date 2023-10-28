e8bebf8f69f9aa9621cebaa04c8aa7ee740d5c74
Cesar Willy Mamani Canaza <cesarwilly.mc@gmail.com>
2023-10-28 13:20:17 -0500

    Add screen shot and activity

--- app/src/main/java/com/cesarwillymc/week2_de_android/MainActivity.kt
+++ app/src/main/java/com/cesarwillymc/week2_de_android/MainActivity.kt
@@ -2,10 +2,36 @@ package com.cesarwillymc.week2_de_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.cesarwillymc.week2_de_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dataBinding: ActivityMainBinding
+    private var arrayList = arrayListOf("Hamburguer", "Pizza", "Mexican", "Chinese")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        dataBinding.btnDecide.setOnClickListener {
+            dataBinding.txtFood.apply {
+                text = arrayList.random()
+            }
+        }
+        dataBinding.btnAddFood.setOnClickListener {
+            if (dataBinding.edFood.isNotBlank()) {
+                addFood(dataBinding.edFood.text.toString())
+                dataBinding.txtFood.text = dataBinding.edFood.text
+            } else {
+                Toast.makeText(this, getString(R.string.lbl_msm_empty), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun EditText.isNotBlank() = this.text.toString().trim().isNotBlank()
+    private fun addFood(text: String) {
+        arrayList.add(text)
     }
 }
---
+++ app/src/main/java/com/cesarwillymc/week2_de_android/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.cesarwillymc.week2_de_android
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Cesar Canaza on 10/28/23.
+ * cesarwilly.mc@gmail.com
+ *
+ * IOWA, United States.
+ */
+class MainViewModel : ViewModel() {
+
+}

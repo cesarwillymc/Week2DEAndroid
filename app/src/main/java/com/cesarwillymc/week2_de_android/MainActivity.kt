d31a8add8ed8b87e8703abddfd0b352bb8fa5da0
Cesar Willy Mamani Canaza <cesarwilly.mc@gmail.com>
2023-10-28 12:59:18 -0500

    Initial commit

---
+++ app/src/main/java/com/cesarwillymc/week2_de_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cesarwillymc.week2_de_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
